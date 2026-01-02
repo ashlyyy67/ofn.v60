a51b55236834a7fee233ba9b4184f051ec7a9a3f
ashly <nadthelf@gmail.com>
2026-01-02 18:34:22 +0300

    Create wrqanb.h

---
+++ wrqanb.h
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>workshop</h2>
+
+<form action="workshop.php" method="post">
+
+  <label for="id">id:</label><br>
+  <input type="text" id="id" name="id" value=""><br>
+  <label for="name">name:</label><br>
+  <input type="text" id="name" name="name" value=""><br><br>
+ 
+    <label for="qualification">qualification:</label><br>
+  <input type="text" id="qualification" name="qualification" value=""><br>
+  <label for="email">email:</label><br>
+  <input type="text" id="email" name="email" value=""><br><br>
+ 
+    <label for="phone">phone:</label><br>
+  <input type="text" id="phone" name="phone" value=""><br>
+  <label for="workshop">workshop:</label><br>
+  <input type="text" id="workshop" name="workshop" value=""><br><br>
+ 
+    <label for="date">date:</label><br>
+  <input type="text" id="date" name="date" value=""><br>
+ 
+  <input type="submit" value="Submit">
+    <input type="reset" value="Cancel">
+</form>
+
+
+</body>
+</html>
